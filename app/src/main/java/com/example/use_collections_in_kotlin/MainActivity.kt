e29035e59f8f463fdab1eed9df5b5746fe00a965
TheShamRio <xsham@inbox.ru>
2024-11-03 12:43:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/use_collections_in_kotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.use_collections_in_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.use_collections_in_kotlin.ui.theme.Use_collections_in_KotlinTheme
+
+fun main() {
+
+}
+
