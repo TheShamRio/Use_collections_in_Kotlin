4fe562426c83cf69e004883ed32def7155d16184
TheShamRio <xsham@inbox.ru>
2024-11-03 12:54:50 +0300

    Lists

--- app/src/main/java/com/example/use_collections_in_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/use_collections_in_kotlin/MainActivity.kt
@@ -14,19 +14,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.use_collections_in_kotlin.ui.theme.Use_collections_in_KotlinTheme
 
 fun main() {
-    val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
-    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
-    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
-    val solarSystem = rockPlanets + gasPlanets
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[0])
-    println(solarSystem[1])
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
     println(solarSystem[2])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+    for (planet in solarSystem) {
+        println(planet)
+    }
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3] = "Future Moon"
     println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
-    println(newSolarSystem[8])
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
 }
 
