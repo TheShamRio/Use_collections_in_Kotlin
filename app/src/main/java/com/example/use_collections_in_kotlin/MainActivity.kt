59f9d8cdf0859c17e078d724f0122c00163f5d76
TheShamRio <xsham@inbox.ru>
2024-11-03 12:49:20 +0300

    Arrays in Kotlin

--- app/src/main/java/com/example/use_collections_in_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/use_collections_in_kotlin/MainActivity.kt
@@ -14,6 +14,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.use_collections_in_kotlin.ui.theme.Use_collections_in_KotlinTheme
 
 fun main() {
-
+    val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
+    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = rockPlanets + gasPlanets
+    solarSystem[3] = "Little Earth"
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
+    println(newSolarSystem[8])
 }
 
