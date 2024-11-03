e9fb46416cc5f5903c27b6054aba69ce598f7811
TheShamRio <xsham@inbox.ru>
2024-11-03 12:58:48 +0300

    Map collection

--- app/src/main/java/com/example/use_collections_in_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/use_collections_in_kotlin/MainActivity.kt
@@ -14,16 +14,25 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.use_collections_in_kotlin.ui.theme.Use_collections_in_KotlinTheme
 
 fun main() {
-    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
     println(solarSystem.size)
-    solarSystem.add("Pluto")
+    solarSystem["Pluto"] = 5
     println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
-    solarSystem.add("Pluto")
-    println(solarSystem.size)
+    println(solarSystem["Pluto"])
+    println(solarSystem.get("Theia"))
     solarSystem.remove("Pluto")
     println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
 }
 
 
